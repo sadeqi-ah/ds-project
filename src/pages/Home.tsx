9c20e65e6604bc2832a1b0e8e2e3f28728dfd94b
amirhossein sadeqi <sadeghi.ah79@gmail.com>
2021-01-17 03:15:44 +0330

    create routing & pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "_/components/Search";
+import StudentList from "_/components/StudentList";
+
+function Home() {
+  return (
+    <>
+      <Search />
+      <StudentList />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Student.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Back from "_/components/Back";
+
+function Student() {
+  return (
+    <>
+      <Back />
+    </>
+  );
+}
+
+export default Student;
